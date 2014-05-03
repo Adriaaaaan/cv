89b8fc03f9a4f46187d25b97a6f41065ba7f9a80
Adrian Meredith <elhobito@gmail.com>
2014-05-03 17:49:05 +0100

    Added initial modalwindow support and email service

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 App = Ember.Application.create();
 
+App.emailAddress='elhobito@gmail.com';
+App.mandrillApiKey = '';
+App.mandrillApiUrl='https://mandrillapp.com/api/1.0/messages/send.json';
+
+/***
+Router
+****/
 App.Router.map(function() {
   // put your routes here
 });
@@ -10,6 +17,9 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+/***
+Components
+****/
  App.BootstrapImgcardComponent = Ember.Component.extend({
     classNames:['col-lg-4']
   });
@@ -18,6 +28,58 @@ App.IndexRoute = Ember.Route.extend({
     classNames:['col-lg-4']
   });
 
-    App.BootstrapDoubleimgcardComponent = App.BootstrapImgcardComponent.extend({
+  App.BootstrapDoubleimgcardComponent = App.BootstrapImgcardComponent.extend({
     classNames:['col-lg-4']
   });
+
+/***
+models
+****/
+App.EmailMessage = Ember.Object.extend({
+	from_email:null,
+	to:[App.EmailRecepient.create({email:App.emailAddress})],
+	autotext:null.
+	subject:null,
+	html:null
+});
+
+/***
+application
+****/
+
+App.ModalWindow = Ember.View.extend({
+	classNames:['modalwindow'],
+	classNameBindings:[visible:showWindow:hideWindow],
+	layoutName:'modalWindow',
+	visible:null,
+	onSuccess:function(){},
+	onError:function(){},
+	close:function(){
+		this.set('visible',false);
+	},
+	actions:{
+		doAction:function(){
+			var self=this;
+			var promise = Ember.RSVP.promise();
+			promise.then(
+				function(response){
+					self.onSuccess(response);
+					this.set('visible',false);
+				},
+				function(error){
+					self.onError(error);
+				}
+			);
+			this.doAction(promise);
+		},
+		cancel:function(){
+			this.close();
+		}
+	}
+});
+
+App.ContactMeView = App.ModalWindow.extend({
+	doAction:function(promise){
+
+	}
+});
